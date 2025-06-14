af5b2e4c9a4d12f0f013f073993860887f93841e
Alan Lanceloth <alan.lanceloth@gmail.com>
2025-06-14 18:46:48 -0300

    feat: implement SQS operations DAG with create, list, send and receive tasks

--- dags/list_sqs.py
+++ dags/list_sqs.py
@@ -20,18 +20,55 @@ def get_sqs_client():
     return client
 
 @dag(
-    dag_id='list_sqs',
-    schedule=None, 
-    start_date=pendulum.datetime(2025,6,14, tz="America/Sao_Paulo"),
+    dag_id='list_sqs',              # mantém seu dag_id
+    schedule=None,
+    start_date=pendulum.datetime(2025, 6, 14, tz="America/Sao_Paulo"),
     catchup=False,
-    tags=['sqs', 'list'],
+    tags=['sqs', 'localstack'],
 )
 def exemplo_sqs():
+    @task
+    def criar_fila():
+        sqs = get_sqs_client()
+        resp = sqs.create_queue(QueueName='create_user')
+        queue_url = resp['QueueUrl']
+        print("Queue criada/recuperada:", queue_url)
+        return queue_url
+
     @task
     def listar_filas():
         sqs = get_sqs_client()
         resp = sqs.list_queues()
-        print(resp.get('QueueUrls'))
-    listar_filas()
+        print("ListQueues:", resp.get('QueueUrls'))
+        return resp.get('QueueUrls') or []
+
+    @task
+    def enviar_mensagem(queue_url: str):
+        sqs = get_sqs_client()
+        resp = sqs.send_message(QueueUrl=queue_url, MessageBody='{"foo":"bar"}')
+        print("SendMessage:", resp)
+        return resp.get('MessageId')
+
+    @task
+    def receber_e_deletar(queue_url: str):
+        sqs = get_sqs_client()
+        resp = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=1)
+        messages = resp.get('Messages') or []
+        if not messages:
+            print("Nenhuma mensagem recebida.")
+            return None
+        for msg in messages:
+            print("Mensagem recebida:", msg)
+            # Deleta após processar
+            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=msg['ReceiptHandle'])
+            print("Mensagem deletada:", msg['MessageId'])
+        return [msg['MessageId'] for msg in messages]
+
+    # Orquestração
+    queue_url = criar_fila()
+    filas = listar_filas()
+    # você pode usar 'filas' se quiser, mas não é necessário para enviar/receber
+    msg_id = enviar_mensagem(queue_url)
+    receber_e_deletar(queue_url)
 
 exemplo_sqs()
